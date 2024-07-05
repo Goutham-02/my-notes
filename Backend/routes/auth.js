d1f99545ebfbdbcbd87f3e12dbe2884c29be84c3
Goutham J <gouthamshastry02@gmail.com>
2024-07-05 19:07:18 +0530

    Login Component Added

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -57,6 +57,7 @@ router.post('/login', [
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password cannot be blank').exists(),
 ], async (req, res) => {
+    let success = false;
     //If there are errors, return Bad request and the errors.
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -67,20 +68,24 @@ router.post('/login', [
 
     try {
         let user = await User.findOne({ email });
-        if (!user)
+        if (!user){
+            success = false;
             return res.status(400).json({ error: "Try to login with correct credentials" });
+        }
 
         const passwordCompare = await bcrypt.compare(password, user.password);
-        if (!passwordCompare)
-            return res.status(400).json({ error: "Try to login with correct credentials" });
-
+        if (!passwordCompare) {
+            success = false;
+            return res.status(400).json({ success, error: "Try to login with correct credentials" });
+        }
         const data = {
             user: {
                 is: user.id
             }
         }
         const authToken = jwt.sign(data, JWT_SECRET);
-        res.json({ authToken });
+        success = true;
+        res.json({ success, authToken });
 
     } catch (error) {
         console.error(error.message);
--- src/App.js
+++ src/App.js
@@ -5,22 +5,24 @@ import About from './components/About';
 import Home from './components/Home';
 import NoteState from './context/notes/NoteState';
 import Alert from './components/Alert';
+import Login from './components/Login.js';
+import Signup from './components/Signup.js';
 
 function App() {
   return (
     <NoteState>
-      <>
-        <Router>
-          <Navbar />
-          <Alert message="This is an alert"/>
-          <div className="container">
-            <Routes>
-              <Route exact path="/" element={<Home />} />
-              <Route exact path="/about" element={<About />} />
-            </Routes>
-          </div>
-        </Router>
-      </>
+      <Router>
+        <Navbar />
+        <Alert message="This is an alert" />
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/signup" element={<Signup />} />
+          </Routes>
+        </div>
+      </Router>
     </NoteState>
   );
 }
